8c3b30ff405276f97d57c1ef0415764efceba5b6
inslutskiy <inslutskiy@gmail.com>
2023-03-14 09:53:00 +0300

    DZ V2.0

--- src/Main.java
+++ src/Main.java
@@ -8,13 +8,7 @@ public class Main {
         List<String> listLower = new ArrayList<>();
 
         while (true){
-            System.out.println("******************************");
-            System.out.println("Выберите операцию:");
-            System.out.println("1 - Добавить продукт в список.");
-            System.out.println("2 - Показать список.");
-            System.out.println("3 - Удалить продукт из списка.");
-            System.out.println("4 - Поиск по списку.");
-            System.out.println("******************************");
+            extracted();
 
             Scanner scanner = new Scanner(System.in);
             int a = scanner.nextInt();
@@ -22,35 +16,22 @@ public class Main {
             switch (a) {
                 case 1:
                     System.out.println("Для добавления введите название.");
-                    Scanner scanner1 = new Scanner(System.in);
-                    String add = scanner1.nextLine();
-                    list.add(add);
+                    list.add(getString());
                     System.out.println("Итого в списке покупок: " + list.size());
                     break;
                 case 2:
-                    System.out.println("******************************");
-                    for (int i = 0; i < list.size(); i++){
-                        System.out.println((i + 1) + ": " + list.get(i));
-                    }
-                    System.out.println("******************************");
+                    extracted(list);
                     break;
                 case 3:
-                    System.out.println("******************************");
                     System.out.println("Введите номер или название товара.");
-                    for (int i = 0; i < list.size(); i++){
-                        System.out.println((i + 1) + ": " + list.get(i));
-                    }
-                    System.out.println("******************************");
+                    extracted(list);
 
-                    Scanner scanner2 = new Scanner(System.in);
-                    String input = scanner2.nextLine();
+                    String input = getString();
 
                     try {
-                        int remI = (Integer.parseInt(input) - 1);
-                        list.remove(remI);
+                        list.remove((Integer.parseInt(input) - 1));
                     } catch (NumberFormatException e) {
-                        String remS = input;
-                        list.remove(remS);
+                        list.remove(input);
                         System.out.println(list);
                         continue;
                     }
@@ -59,13 +40,11 @@ public class Main {
                 case 4:
                     for (int i = 0; i < list.size(); i++) {
                         String b = list.get(i);
-                        String c = b.toLowerCase();
-                        listLower.add(c);
+                        listLower.add(b.toLowerCase());
                     }
 
                     System.out.println("Для поиска введите название.");
-                    Scanner scanner3 = new Scanner(System.in);
-                    String search = scanner3.nextLine().toLowerCase();
+                    String search = getString().toLowerCase();
 
                     for (int i = 0; i < listLower.size(); i++) {
                         String product = listLower.get(i);
@@ -79,6 +58,28 @@ public class Main {
                     System.out.println("Неверный номер действия!");
                     break;
             }
+        }
+    }
+
+    private static String getString() {
+        Scanner scannerS = new Scanner(System.in);
+        String enteredData = scannerS.nextLine();
+        return enteredData;
+    }
+
+    private static void extracted() {
+        System.out.println("------------------------------");
+        System.out.println("Выберите операцию:");
+        System.out.println("1 - Добавить продукт в список.");
+        System.out.println("2 - Показать список.");
+        System.out.println("3 - Удалить продукт из списка.");
+        System.out.println("4 - Поиск по списку.");
+    }
+
+    private static void extracted(List<String> list) {
+        System.out.println("------------------------------");
+        for (int i = 0; i < list.size(); i++){
+            System.out.println((i + 1) + ": " + list.get(i));
         }
     }
 }
